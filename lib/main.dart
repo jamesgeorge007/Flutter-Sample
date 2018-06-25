f4e32001a0d45977e14e05f4f6f1a4a95fa796f8
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-06-25 15:30:37 +0530

    fetch data from API section being implemented which is being rendered as listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,72 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(new RealWorldApp());
 
-class RealWorldApp extends StatefulWidget{
+class RealWorldApp extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return new RealWorldState();
-     }
+  State<StatefulWidget> createState() {
+    return new RealWorldState();
+  }
 }
 
-class RealWorldState extends State<RealWorldApp>{
+class RealWorldState extends State<RealWorldApp> {
   var _isLoading = true;
-  @override
-    Widget build(BuildContext context) {
-      return new MaterialApp(
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Sample Text"),
-            actions: <Widget>[
-              new IconButton(icon: new Icon(Icons.refresh),
-              onPressed: (){
-                 print('Reloading triggered!');
-                 setState(() {
-                           _isLoading = false;        
-                                  });
-              }, )
-            ],
-          ),
-          body: new Center(
-            child: _isLoading ? new CircularProgressIndicator() : new Text("Finished loading!")
-          )
-        )
-      );
+  var videos;
+
+  _fetchData() async {
+    final url = 'https://api.letsbuildthatapp.com/youtube/home_feed';
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final map = json.decode(response.body);
+      final videosJson = map["videos"];
+
+      setState(() {
+        _isLoading = false;
+        this.videos = videosJson;
+      });
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: new Scaffold(
+            appBar: new AppBar(
+              title: new Text("Sample Text"),
+              actions: <Widget>[
+                new IconButton(
+                  icon: new Icon(Icons.refresh),
+                  onPressed: () {
+                    print('Reloading triggered!');
+                    _fetchData();
+                  },
+                )
+              ],
+            ),
+            body: new Center(
+                child: _isLoading
+                    ? new CircularProgressIndicator()
+                    : new ListView.builder(
+                        itemCount: this.videos.length,
+                        itemBuilder: (context, i) {
+                          final video = this.videos[i];
+                          return new Column(
+                            children: <Widget>[
+                              new Container(
+                                padding: new EdgeInsets.all(16.0),
+                                child: new Column(
+                                  children: <Widget>[
+                                  new Image.network(video["imageUrl"]),
+                                  new Text(video["name"]),
+                                  ]
+                                ),
+                              ),
+                              new Divider()
+                            ],
+                          );
+                        }))));
+  }
 }
